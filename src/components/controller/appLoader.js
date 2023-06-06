6e471f9857a17960a8d424692cae5c24f6476231
JaKsHaZbi <Joker_hazbi@mail.ru>
2023-06-06 11:20:22 +0300

    init: start new task with my apiKey

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'bf5dbde6b69a4b5abd87d3345fdaf8e9', // получите свой ключ https://newsapi.org/
         });
     }
 }
